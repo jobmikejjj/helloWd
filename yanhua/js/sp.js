9468952d58c64ec07dc7b6ad42cd9713b8b4e0da
jobmikejjj <152639741+jobmikejjj@users.noreply.github.com>
2023-12-02 01:06:36 +0800

    Add files via upload

---
+++ yanhua/js/sp.js
@@ -0,0 +1,31 @@
+// 商品数组
+var products = [
+    { image: './images/1.png',video: './videos/1.mp4', price: 68 ,name:'加特林'},
+    { image: './images/2.png',video: './videos/2.mp4', price: 150 ,name:'大礼包'},
+    { image: './images/3.png',video: './videos/1.mp4', price: 25 ,name:'20发礼花/0.9'},
+    { image: './images/4.png',video: './videos/1.mp4', price: 40 ,name:'36发礼花带笛音/0.9'},
+    { image: './images/5.png',video: './videos/1.mp4', price: 30 ,name:'金焰三分钟'},
+    { image: './images/5.png',video: './videos/1.mp4', price: 35 ,name:'笑笑三分钟'},
+    { image: './images/6.png',video: './videos/1.mp4', price: 30 ,name:'孔雀开屏'},
+    { image: './images/7.png',video: './videos/1.mp4', price: 30,name:'星空水母'},
+    { image: './images/8.png',video: './videos/1.mp4', price: 35 ,name:'转转乐'},
+    { image: './images/9.png',video: './videos/1.mp4', price: 10 ,name:'黑老大（10）'},
+    { image: './images/10.png',video: './videos/1.mp4', price: 20 ,name:'020火箭（10）'},
+    { image: './images/11.png',video: './videos/1.mp4', price: 20 ,name:'满地珍珠（10）'},
+    { image: './images/12.png',video: './videos/1.mp4', price: 18 ,name:'金丝雨（2）'},
+    { image: './images/13.png',video: './videos/1.mp4', price: 18 ,name:'雪花飞舞（2）'},
+    { image: './images/14.png',video: './videos/1.mp4', price:  25,name:'多彩魔鞭'},
+    { image: './images/15.png',video: './videos/1.mp4', price: 25 ,name:'金花银雨'},
+    { image: './images/16.png',video: './videos/1.mp4', price: 30 ,name:'开心转盘'},
+    { image: './images/17.png',video: './videos/1.mp4', price: 30 ,name:'雷神坦克'},
+    { image: './images/18.png',video: './videos/1.mp4', price: 25 ,name:'笑春风'},
+    { image: './images/19.png',video: './videos/1.mp4', price: 35 ,name:'年年有鱼'},
+    { image: './images/20.png',video: './videos/1.mp4', price:  35,name:'大闪亮美人（100发）'},
+    { image: './images/21.png',video: './videos/1.mp4', price: 25 ,name:'照明弹（4）'},
+    { image: './images/22.png',video: './videos/1.mp4', price: 18 ,name:'飞毛腿（4）'},
+    { image: './images/23.png',video: './videos/1.mp4', price:  15,name:'8层条鞭'},
+    { image: './images/23.png',video: './videos/1.mp4', price:  20,name:'10层条鞭'},
+    { image: './images/24.png',video: './videos/1.mp4', price: 40 ,name:'40公分红炮'},
+
+    
+];
