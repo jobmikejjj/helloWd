55b34782d269d0c76e4deb0994aabc935d5b4dd6
jobmikejjj <152639741+jobmikejjj@users.noreply.github.com>
2023-12-09 15:01:56 +0800

    Update sp.js

--- js/sp.js
+++ js/sp.js
@@ -12,7 +12,7 @@ var products = [
     { image: './images/9.png',video: './videos/1.mp4', price: 10 ,name:'黑老大（10）'},
     { image: './images/10.png',video: './videos/020huoj.mp4', price: 20 ,name:'020火箭（10）'},
     { image: './images/11.png',video: './videos/mandizz.mp4', price: 20 ,name:'满地珍珠（10）'},
-    { image: './images/12.png',video: './videos/jinsiyu.mp4', price: 18 ,name:'金丝雨（2）'},
+    { image: './images/12.png',video: './videos/jinsiyu.mp4', price: 18 ,name:'金丝雨（4）'},
     { image: './images/13.png',video: './videos/xuehuafeiwu.mp4', price: 18 ,name:'雪花飞舞（2）'},
     { image: './images/14.png',video: './videos/duocaimb.mp4', price:  25,name:'多彩魔鞭'},
     { image: './images/15.png',video: './videos/jinhuayy.mp4', price: 25 ,name:'金花银雨'},
