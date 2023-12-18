f4f17336ffe6f63d3ec47084c92b85144ec13a5b
jobmikejjj <152639741+jobmikejjj@users.noreply.github.com>
2023-12-19 00:58:13 +0800

    Add files via upload

---
+++ js/singleSP.js
@@ -0,0 +1,31 @@
+// 商品数组
+var products = [
+    {  price: 40 ,name:'加特林'},
+    {  price: 120 ,name:'大礼包'},
+    {  price: 15 ,name:'20发礼花/0.9'},
+    {  price: 30 ,name:'36发礼花带笛音/0.9'},
+    {  price: 20 ,name:'金焰三分钟'},
+    {  price: 22 ,name:'笑笑三分钟'},
+    {  price: 12 ,name:'孔雀开屏'},
+    {  price: 14,name:'星空水母'},
+    {  price: 22 ,name:'转转乐'},
+    {  price: 5 ,name:'黑老大（10）'},
+    {  price: 10 ,name:'020火箭（10）'},
+    {  price: 10 ,name:'满地珍珠（10）'},
+    {  price: 10 ,name:'金丝雨（4）'},
+    {  price: 8 ,name:'雪花飞舞（2）'},
+    {  price: 12,name:'多彩魔鞭'},
+    {  price: 13 ,name:'金花银雨'},
+    {  price: 20 ,name:'开心转盘'},
+    {  price: 20 ,name:'雷神坦克'},
+    {  price: 15 ,name:'笑春风'},
+    {  price: 25 ,name:'年年有鱼'},
+    {  price: 20,name:'大闪亮美人（100发）'},
+    {  price: 12 ,name:'照明弹（4）'},
+    {  price: 10 ,name:'飞毛腿（4）'},
+    {  price: 8,name:'8层条鞭'},
+    {  price: 10,name:'10层条鞭'},
+    {  price: 30 ,name:'40公分红炮'},
+
+    
+];
